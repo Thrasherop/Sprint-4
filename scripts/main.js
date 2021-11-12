753a4a89b9383345170c26e65df1b0a5004110fd
Thrasherop <ultraprim@gmail.com>
2021-11-11 17:58:44 -0700

    made sure there was enough lines of code

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,23 @@
 //document.getElementById("hello_world").textContent = "Other hello world yopoooooooo";
 
+// Gets a random number between min and max
 function getRandomByRange(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
+// sets the event listener for the button
+// when the button is pressed, set the dice number
+// to a random D6 number
 document.getElementById("button").addEventListener("click", () => {
 
-
-
+    // get the dice number
     let randomNumber = getRandomByRange(1, 6);
 
+    // set the dice number
     document.getElementById("output").textContent = randomNumber;
 
+    // Logs the roll
+    console.log(randomNumber);
 
 });
 
